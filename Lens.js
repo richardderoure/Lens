8906f27c5de5396f15b94876aed5b0d528b35d7d
richard <r.d.deroure@se16.qmul.ac.uk>
2018-12-02 19:29:28 +0000

    2nd commit

--- Lens.js
+++ Lens.js
@@ -1,5 +1,9 @@
 console.log("loaded");
+
 const canvas = document.getElementById("canvas");
+canvas.width = 1200;
+canvas.height = 675;
+
 const ctx = canvas.getContext("2d");
 
 function getMouseCoordinates(canvas, event) {
@@ -22,45 +26,6 @@ function isStarHere(stars, x, y) {
 }
 
 
-const stars = [
-	{
-  	x: 20,
-    y: 40,
-    radius: 5,
-    fillStyle: "blue",
-    mass: 5,
-    dS: 10,
-    dL: 10,
-  },
-  {
-  	x: 140,
-    y: 100,
-    radius: 5,
-    fillStyle: "white",
-    mass: 5,
-    dS: 10,
-    dL: 10,
-  },
-  {
-  	x: 190,
-    y: 60,
-    radius: 5,
-    fillStyle: "green",
-    mass: 5,
-    dS: 10,
-    dL: 10,
-  },
-  {
-  	x: 250,
-    y: 30,
-    radius: 5,
-    fillStyle: "pink",
-    mass: 5,
-    dS: 10,
-    dL: 10,
-  },
-];
-
 function drawStars(stars) {
 	const canvas = document.getElementById("canvas");
 	const ctx = canvas.getContext("2d");
@@ -68,9 +33,9 @@ function drawStars(stars) {
 	stars.forEach(star => {
   	ctx.beginPath();
     ctx.arc(star.x, star.y, star.radius, 0, 2 * Math.PI);
-    ctx.fillStyle = star.fillStyle;
+    ctx.fillStyle = star.colour;
     ctx.fill();
-  	ctx.stroke();
+  	//ctx.stroke();
   });
 }
 
@@ -88,9 +53,17 @@ canvas.addEventListener("mousemove", function(event) {
   if (foundStar) {
   	console.log("Found a star!");
     ctx.beginPath();
-    ctx.arc(mouseLocation.x, mouseLocation.y, 10, 0, 2 * Math.PI);
-    ctx.stroke();    
-  }
+    ctx.arc(mouseLocation.x, mouseLocation.y, 20, 0, 2 * Math.PI);
+    ctx.strokeStyle = foundStar.colour;
+    ctx.stroke();
+   
+
+
+  } 
+const value = document.getElementById("value")
+value.textContent = `${mouseLocation.x}, ${mouseLocation.y - 0.125}`;
+//console.log(mouseLocation.x,mouseLocation.y);
+
 });
 
 
